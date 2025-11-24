daaca26b0f41d0c7740c360e245cac566986f7e6
Oscar Gjerde <oscargjerde@Oscar-sin-MacBook-Air.local>
2025-11-24 12:07:15 +0100

    Debug: Add test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: 50, fontFamily: 'sans-serif' }}>
+            <h1>Test Page</h1>
+            <p>If you can see this, the deployment is working!</p>
+        </div>
+    );
+}
