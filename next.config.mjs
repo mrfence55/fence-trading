9c1427f648b5501811d103cc511774dfd26d1053
Oscar Gjerde <oscargjerde@Oscar-sin-MacBook-Air.local>
2026-01-05 12:02:59 +0100

    FIX: syntax error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-experimental: {
-    serverComponentsExternalPackages: ["better-sqlite3"],
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ["better-sqlite3"],
     },
+};
 
 export default nextConfig;
