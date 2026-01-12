d8de3404b803cae4d126182028224510b710fe9a
Oscar Gjerde <oscargjerde@Oscar-sin-MacBook-Air.local>
2026-01-13 00:06:32 +0100

    fix: restore 'use client' directive

--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import { SignalTable, Signal } from "@/components/SignalTable";
 // import { PerformanceStats } from "@/components/PerformanceStats";
